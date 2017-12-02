3312b75abf452383cee9c8c93196ffa8194d5e39
grubnubble <montanalisagoodman@gmail.com>
2017-12-02 10:51:33 -0500

    Conditional rendering for meme me button vs meme cards

--- App.js
+++ App.js
@@ -9,23 +9,34 @@ export default class App extends React.Component {
     memes: [],
   }
   //could move fetch here instead of having input component
+  //TODO: why do i have to use Views? why not divs?
+
 
   render() {
     // console.log("app.js memes: ", this.state.memes)
+
+    //conditional for returning the input or the cards
+    let display;
+    if (this.state.memes.length >0) {
+      display = <Tinder memes={this.state.memes}/>
+    } else {
+      display = <View>        
+                      <View style={styles.container}>
+                          <Text style={styles.text}>Welcome to Meemstr!</Text>
+                          <Text style={styles.text}>The true meeming of love</Text>
+                        </View>
+
+                        <Input 
+                          updateMemes={(memes) => {this.setState({ memes: memes }); } }/>
+                      </View>;
+    }
+
+
     return (
       <View style={styles.container}>
 
-          <View style={styles.container}>
-            <Text style={styles.text}>Welcome to Meemstr!</Text>
-            <Text style={styles.text}>The true meeming of love</Text>
-          </View>
+          {display}
 
-          <Input 
-            updateMemes={(memes) => this.setState({ memes: memes })}/>
-
-          <Tinder 
-            memes={this.state.memes}
-            />
       </View>
     );
   }
